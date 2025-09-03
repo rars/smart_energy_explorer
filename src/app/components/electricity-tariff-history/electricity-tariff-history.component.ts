f82982cfa3aad42aec9453511477986f3e10a1e7
Richard Russell <2265225+rars@users.noreply.github.com>
2025-09-03 11:53:02 +0100

    feat: improve tariff views - adding sorting and handling overflow

--- src/app/components/electricity-tariff-history/electricity-tariff-history.component.ts
+++ src/app/components/electricity-tariff-history/electricity-tariff-history.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 import { from } from 'rxjs';
 
@@ -17,10 +18,10 @@ export interface UnitPrice {
 }
 
 @Component({
-    selector: 'app-electricity-tariff-history',
-    imports: [CommonModule, MatTableModule],
-    templateUrl: './electricity-tariff-history.component.html',
-    styleUrl: './electricity-tariff-history.component.scss'
+  selector: 'app-electricity-tariff-history',
+  imports: [CommonModule, MatTableModule, MatSortModule],
+  templateUrl: './electricity-tariff-history.component.html',
+  styleUrl: './electricity-tariff-history.component.scss',
 })
 export class ElectricityTariffHistoryComponent implements OnInit {
   public readonly displayedStandingChargeColumns = [
@@ -32,8 +33,20 @@ export class ElectricityTariffHistoryComponent implements OnInit {
     'unitPricePence',
   ];
 
-  public standingChargesDataSource: StandingCharge[] | undefined = undefined;
-  public unitPricesDataSource: UnitPrice[] | undefined = undefined;
+  public standingChargesDataSource?: MatTableDataSource<StandingCharge> =
+    undefined;
+  public unitPricesDataSource?: MatTableDataSource<UnitPrice> = undefined;
+
+  @ViewChild('standingChargesSort') set standingChargesSort(sort: MatSort) {
+    if (this.standingChargesDataSource) {
+      this.standingChargesDataSource.sort = sort;
+    }
+  }
+  @ViewChild('unitPricesSort') set unitPricesSort(sort: MatSort) {
+    if (this.unitPricesDataSource) {
+      this.unitPricesDataSource.sort = sort;
+    }
+  }
 
   public ngOnInit(): void {
     from(
@@ -42,19 +55,23 @@ export class ElectricityTariffHistoryComponent implements OnInit {
         unitPrices: { priceEffectiveTime: string; unitPricePence: number }[];
       }>('get_electricity_tariff_history', {}),
     ).subscribe((data) => {
-      this.standingChargesDataSource = data?.standingCharges?.map((x) => {
-        return {
-          startDate: new Date(x.startDate),
-          standingChargePence: x.standingChargePence,
-        };
-      });
+      this.standingChargesDataSource = new MatTableDataSource<StandingCharge>(
+        data?.standingCharges?.map((x) => {
+          return {
+            startDate: new Date(x.startDate),
+            standingChargePence: x.standingChargePence,
+          };
+        }) ?? [],
+      );
 
-      this.unitPricesDataSource = data?.unitPrices?.map((x) => {
-        return {
-          priceEffectiveTime: new Date(x.priceEffectiveTime),
-          unitPricePence: x.unitPricePence,
-        };
-      });
+      this.unitPricesDataSource = new MatTableDataSource<UnitPrice>(
+        data?.unitPrices?.map((x) => {
+          return {
+            priceEffectiveTime: new Date(x.priceEffectiveTime),
+            unitPricePence: x.unitPricePence,
+          };
+        }) ?? [],
+      );
     });
   }
 }
--- src/app/components/gas-tariff-history/gas-tariff-history.component.ts
+++ src/app/components/gas-tariff-history/gas-tariff-history.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 import { from } from 'rxjs';
 
@@ -17,10 +18,10 @@ export interface UnitPrice {
 }
 
 @Component({
-    selector: 'app-gas-tariff-history',
-    imports: [CommonModule, MatTableModule],
-    templateUrl: './gas-tariff-history.component.html',
-    styleUrl: './gas-tariff-history.component.scss'
+  selector: 'app-gas-tariff-history',
+  imports: [CommonModule, MatTableModule, MatSortModule],
+  templateUrl: './gas-tariff-history.component.html',
+  styleUrl: './gas-tariff-history.component.scss',
 })
 export class GasTariffHistoryComponent {
   public readonly displayedStandingChargeColumns = [
@@ -32,8 +33,20 @@ export class GasTariffHistoryComponent {
     'unitPricePence',
   ];
 
-  public standingChargesDataSource: StandingCharge[] | undefined = undefined;
-  public unitPricesDataSource: UnitPrice[] | undefined = undefined;
+  public standingChargesDataSource?: MatTableDataSource<StandingCharge> =
+    undefined;
+  public unitPricesDataSource?: MatTableDataSource<UnitPrice> = undefined;
+
+  @ViewChild('standingChargesSort') set standingChargesSort(sort: MatSort) {
+    if (this.standingChargesDataSource) {
+      this.standingChargesDataSource.sort = sort;
+    }
+  }
+  @ViewChild('unitPricesSort') set unitPricesSort(sort: MatSort) {
+    if (this.unitPricesDataSource) {
+      this.unitPricesDataSource.sort = sort;
+    }
+  }
 
   public ngOnInit(): void {
     from(
@@ -42,19 +55,23 @@ export class GasTariffHistoryComponent {
         unitPrices: { priceEffectiveTime: string; unitPricePence: number }[];
       }>('get_gas_tariff_history', {}),
     ).subscribe((data) => {
-      this.standingChargesDataSource = data?.standingCharges?.map((x) => {
-        return {
-          startDate: new Date(x.startDate),
-          standingChargePence: x.standingChargePence,
-        };
-      });
+      this.standingChargesDataSource = new MatTableDataSource(
+        data?.standingCharges?.map((x) => {
+          return {
+            startDate: new Date(x.startDate),
+            standingChargePence: x.standingChargePence,
+          };
+        }) ?? [],
+      );
 
-      this.unitPricesDataSource = data?.unitPrices?.map((x) => {
-        return {
-          priceEffectiveTime: new Date(x.priceEffectiveTime),
-          unitPricePence: x.unitPricePence,
-        };
-      });
+      this.unitPricesDataSource = new MatTableDataSource(
+        data?.unitPrices?.map((x) => {
+          return {
+            priceEffectiveTime: new Date(x.priceEffectiveTime),
+            unitPricePence: x.unitPricePence,
+          };
+        }) ?? [],
+      );
     });
   }
 }
