f013f74100de6962b8a21500c3f16e1a23118259
Richard Russell <2265225+rars@users.noreply.github.com>
2026-03-27 17:13:22 +0000

    refactor: remove zone.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,25 @@
-import {
-  ApplicationConfig,
-  ErrorHandler,
-  provideZoneChangeDetection,
-} from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { enGB } from 'date-fns/locale';
 
-import { GlobalErrorHandler } from './services/error/global-error-handler';
-import { routes } from './app.routes';
 import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  ApplicationConfig,
+  ErrorHandler,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideDateFnsAdapter } from '@angular/material-date-fns-adapter';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
-import { enGB } from 'date-fns/locale';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+
+import { routes } from './app.routes';
+import { GlobalErrorHandler } from './services/error/global-error-handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimationsAsync(),
